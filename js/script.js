d9bd114170373d0c5784f442c78864c72dd7a78f
dannyblck <Amirkhanov.Official@gmail.com>
2020-07-31 01:52:56 +0600

    ready

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
-    $('.carousel_inner').slick({
+    $('.reviews_carousel').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png"></img></button>',
+        autoplay: true,
+        pauseOnHover: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowleft_reviews.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowright_reviews.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
+    const menu = document.querySelector('.promo_navigation'),
+    menuItem = document.querySelectorAll('.promo_navigation_item'),
     hamburger = document.querySelector('.hamburger');
-
+  
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+        menu.classList.toggle('promo_navigation_active');
     });
-
+  
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+            menu.classList.toggle('promo_navigation_active');
         })
     });
 });
-
-// $('.carousel_inner').slick({
-//     centerMode: true,
-//     centerPadding: '60px',
-//     slidesToShow: 1,
-//     arrows: true,
-//   });
